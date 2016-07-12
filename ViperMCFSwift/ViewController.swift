ce9540b476c17dc3009c3d2450c9437106abf453
SpectralDragon <spectraldragonchannel@gmail.com>
2016-07-12 16:15:12 +0300

    Initial Commit

---
+++ ViperMCFSwift/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  ViperMCFSwift
+//
+//  Created by Владислав on 12.07.16.
+//  Copyright © 2016 WebAnt. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
